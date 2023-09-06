2ac4222497f4bc2c9fcae4d90810aa5d65cc063f
Giorgi Epitashvili <devstudio.corp@gmail.com>
2023-09-06 13:54:58 +0400

    32. More Validation Rules

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -15,13 +15,19 @@ class LoginController extends Controller
     public function handleLogin (Request $request)
     {
         // dd($request->all());
+
+
+        // alfa - input must only contain only letters
+        // min6 - input must contain minimum 6 characters
+        // max:15 - input must contain maximum 15 characters
+        // email - The email field must be a valid email address
         $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'name' => ['required', 'alpha', 'min:6', 'max:15'],
+            'email' =>  ['required', 'email'],
+            'password' =>  ['required']
         ]);
 
         return $request;
     }
 }
-                             
+                                 
