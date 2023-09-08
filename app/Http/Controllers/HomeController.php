46abb1bc099345530ffa9207011eccb446610398
Giorgi Epitashvili <devstudio.corp@gmail.com>
2023-09-08 10:33:51 +0400

    51. Retrieving/get Records Via Eloquent Models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,7 +18,24 @@ class HomeController extends Controller
        /*                                Eloquent ROM                                */
        /* -------------------------------------------------------------------------- */
 
-       // all() == get() ||| get all data from Posts table
-       return $posts = Post::all();
+       // all() == get() ||| get all data from Posts table |||  === DB::table('posts')->get();  ||| Returns an array[]
+       // return Post::all(); 
+
+       // get current single row data from table with id=17 ||| Returns an array[]
+       // return Post::find(17);
+
+       // get "title" field from table name with id=17 ||| Returns an array[]
+       // $post =  Post::find(17);
+       // return $post->title;
+
+       // get current single row data from table with id=898 ||| If no matching model exist, it throws an error1. ||| Returns an array[]
+       // return Post::findorFail(898);
+
+
+       // Extract data from received array
+       $posts = Post::all(); 
+       foreach($posts as $post) {
+           echo $post->title . "<br>";
+       }
     }
 }
