d8926f5fa253eac0e16fb361b37a86e5c95de394
Giorgi Epitashvili <devstudio.corp@gmail.com>
2023-09-11 12:33:57 +0400

    70. delete/Remove Files from Storage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\DB;
+
+
+
+use Illuminate\Support\Facades\Storage;
+
+use File;
 
 class HomeController extends Controller
 {
@@ -15,10 +20,27 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
        /* -------------------------------------------------------------------------- */
-       /*                                 UPLOAD FILE                                */
+       /*                                 DELETE FILE                                */
        /* -------------------------------------------------------------------------- */
 
 
-        return view('home');
+       /* ------------------------- DELETE file - Option 1: ------------------------ */
+
+       // find 'public' path --> delete '/images/myimage.png' from it - the best option !!!
+       // Storage::disk('public')->delete('/images/myimage.png');
+
+
+
+        /* ------------------------- DELETE file - Option 2: ------------------------ */
+
+       // storage_path() ==> "C:\MAMP\htdocs\Laravel-Udemy\storage"
+       // File::delete(storage_path("/app/public/images/image1.png")); 
+
+       
+
+       /* ------------------------- DELETE file - Option 3: ------------------------ */
+       // unlink() method
+       // unlink(storage_path("/app/public/images/image2.png"));
+
     }
 }
