df302a4ceb256f941bc194feb39bf4d6b0d989c6
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-10-21 14:24:25 +0400

    91-2 Dependency Injection - create DI method 2

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -1,11 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Services\SimpleService;
 use Illuminate\Http\Request;
 
+// DI - Method 2
 class SampleController extends Controller
 {
+    private $simpleService;
+    public function __construct(SimpleService $simpleService) {
+        $this->simpleService = $simpleService;
+    }
     public function index(Request $request) {
+        $this->simpleService->log("This is a Test log");
+        $this->anotherMethod();
+        
         return $request->all();
+    }
+    public function anotherMethod() {
+        $this->simpleService->log("Hello World - LOG");
     }
 }
